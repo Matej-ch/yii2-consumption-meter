1de1065c996a5b6200a7e359b6de06fe71a5d962
Matej <matejchalachan@gmail.com>
2022-01-24 18:22:15 +0100

    added menu and notes to js

--- src/views/iot24-price-map/create.php
+++ src/views/iot24-price-map/create.php
@@ -2,16 +2,32 @@
 
 /* @var $this yii\web\View */
 
-$this->title = Yii::t('iot24meter/msg', 'year');
+use matejch\iot24meter\assets\CalendarAsset;
 
-\matejch\iot24meter\assets\CalendarAsset::register($this);
+$this->title = 'Kalendár';
+
+CalendarAsset::register($this);
 
 ?>
 
 <div class="js-year w-full">
 
     <div class="py-2 px-2 font-bold text-3xl">
-        <div id="calendar" style="height: 100vh"></div>
+
+        <div id="menu">
+            <span id="menu-navi">
+                <button type="button" class="btn btn-default btn-sm move-today" data-action="move-today">Dnes</button>
+                <button type="button" class="btn btn-default btn-sm move-day" data-action="move-prev">
+                    <i class="calendar-icon ic-arrow-line-left" data-action="move-prev"></i>
+                </button>
+                <button type="button" class="btn btn-default btn-sm move-day" data-action="move-next">
+                    <i class="calendar-icon ic-arrow-line-right" data-action="move-next"></i>
+                </button>
+            </span>
+            <span id="renderRange" class="render-range"></span>
+        </div>
+
+        <div id="calendar"></div>
     </div>
 
 
