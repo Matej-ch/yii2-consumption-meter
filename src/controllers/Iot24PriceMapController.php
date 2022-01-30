8f7391138497b9bd9e511894ee52f75ef000a347
Matej <matejchalachan@gmail.com>
2022-01-30 12:33:35 +0100

    expor xsls with interavals and months , days missing

--- src/controllers/Iot24PriceMapController.php
+++ src/controllers/Iot24PriceMapController.php
@@ -35,34 +35,51 @@ class Iot24PriceMapController extends \yii\web\Controller
         $year = date('Y');
         $calendar = [];
         foreach ($months as $month) {
+
+            $monthName = \DateTime::createFromFormat('!m', $month);
+            $monthName = $monthName->format('F');
+
             for ($d = 1; $d <= 31; $d++) {
                 $time = mktime(12, 0, 0, $month, $d, $year);
                 if ((int)date('m', $time) === $month) {
-                    $calendar[$month][$d]['name'] = date('l', $time);
-                    $calendar[$month][$d]['full_date'] = date('Y-m-d', $time);
+                    $calendar[$monthName][$d]['name'] = date('l', $time);
+                    $calendar[$monthName][$d]['full_date'] = date('Y-m-d', $time);
 
                     $startTime = new \DateTime(date('Y-m-d 00:00:00', $time));
                     $endTime = new \DateTime(date('Y-m-d 24:00:00', $time));
                     while ($startTime < $endTime) {
-                        $calendar[$month][$d]['intervals'][] = $startTime->modify('+15 minutes')->format('H:i:s');
+                        $calendar[$monthName][$d]['intervals'][] = $startTime->modify('+15 minutes')->format('H:i:s');
                     }
                 }
             }
         }
 
-        foreach ($calendar as $monthIndex => $month) {
-            $worksheet = $spreadsheet->addSheet(new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, ''));
-        }
+        foreach ($calendar as $monthName => $days) {
 
+            $worksheet = $spreadsheet->addSheet(new \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet($spreadsheet, $monthName));
+
+            $temp = 2;
+            foreach ($days[1]['intervals'] as $i => $interval) {
+                if ($i === 0) {
+                    $worksheet->setCellValue("A{$temp}", $days[1]['intervals'][count($days[1]['intervals']) - 1]);
+                    $worksheet->setCellValue("B{$temp}", $interval);
+                    continue;
+                }
+
+                $worksheet->setCellValue("A{$temp}", $days[1]['intervals'][$i - 1]);
+                $worksheet->setCellValue("B{$temp}", $interval);
+                $temp++;
+            }
+        }
+        $spreadsheet->removeSheetByIndex(0);
 
         $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
         $writer->save("calendar.xlsx");
 
-        //header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        //header('Content-Disposition: attachment; filename="'. urlencode("calendar.xlsx").'"');
-        //$writer->save('php://output');
-        //exit();
-        return $this->redirect(Yii::$app->request->referrer);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment; filename="' . urlencode("calendar.xlsx") . '"');
+        $writer->save('php://output');
+        exit();
     }
 
     public function actionImport(): Response
