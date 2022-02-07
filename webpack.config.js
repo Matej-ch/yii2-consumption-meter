176176f4f9fe8948c4002e84c802660ea9eef029
Matej <matejchalachan@gmail.com>
2022-02-07 22:56:08 +0100

    adding webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const mode = process.env.NODE_ENV === 'production' ? 'production' : "development";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
     mode: mode,
-
+    entry: [
+        './src/web/css/main.css'
+    ],
+    output: {
+        filename: './src/web/css/[name].min.css',
+    },
     module: {
         rules: [
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
             /*{
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -13,10 +24,44 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             },*/
-            {
-                test: /\.(s[ac]|c)ss$/, //css , scss, sass files
-                use: ["css-loader", "postcss-loader"]
-            },
+            /* {
+                 test: /\.(s[ac]|c)ss$/, //css , scss, sass files
+                 use: ["css-loader", "postcss-loader"]
+             },*/
+            /*{
+                test: /\.(s[ac]|c)ss$/i,
+
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: ""}
+                    },
+                    "css-loader", "postcss-loader", "sass-loader"],
+            }*/
         ]
     },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            //`...`,
+            new CssMinimizerPlugin({
+                include: /\/src\/web\/css/,
+                test: /\.css(\?.*)?$/i,
+                minify: [
+                    CssMinimizerPlugin.cssnanoMinify,
+                    CssMinimizerPlugin.cleanCssMinify
+                ],
+                minimizerOptions: {
+                    preset: [
+                        "default",
+                        {
+                            discardComments: {removeAll: true},
+                        },
+                    ],
+                }
+            }),
+        ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
 }
