cd5a4182e974b44752a433b8bd5d02733ef02b5e
Matej <matejchalachan@gmail.com>
2022-02-21 15:30:27 +0100

    added devices

--- src/enums/Device.php
+++ src/enums/Device.php
@@ -4,16 +4,24 @@ namespace matejch\iot24meter\enums;
 
 class Device
 {
-    public CONST ELEKTROMETER = 'elektrometer';
-    public CONST ELEKTROMETER_CHODBA = 'elektrometer_chodba';
-    public CONST POTRUBIE = 'potrubie';
+    public const ELEKTROMETER = 'elektrometer';
+    public const ELEKTROMETER_CHODBA = 'elektrometer_chodba';
+    public const POTRUBIE = 'potrubie';
+    public const ELEKTROMETER_3 = "elektrometer 3";
+    public const ELEKTROMETER_3_CHODBA = "elektrometer3 chodba";
+    public const ELEKTROMETER_5 = "elektrometer 5";
+    public const ELEKTROMETER_6 = "elektrometer 6";
 
     public static function getList(): array
     {
         return [
             self::ELEKTROMETER => 'elektrometer',
             self::ELEKTROMETER_CHODBA => 'elektrometer_chodba',
-            self::POTRUBIE => 'potrubie'
+            self::POTRUBIE => 'potrubie',
+            self::ELEKTROMETER_3 => 'elektrometer 3',
+            self::ELEKTROMETER_3_CHODBA => 'elektrometer3 chodba',
+            self::ELEKTROMETER_5 => 'elektrometer 5',
+            self::ELEKTROMETER_6 => 'elektrometer 6',
         ];
     }
 
