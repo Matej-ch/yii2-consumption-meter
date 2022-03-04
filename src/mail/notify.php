c2261f44743944083fedadf0dc7fb1675b481d90
Matej <matejchalachan@gmail.com>
2022-03-04 21:52:22 +0100

    notification mail html fixed

--- src/mail/notify.php
+++ src/mail/notify.php
@@ -5,8 +5,11 @@
 
 ?>
 <!--[if (gte mso 9)|(IE)]>
-<table width='800' align='center'><tr><td><![endif]-->
-<table border='0' cellpadding='0' cellspacing='0' width='100%' style='max-width:800px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif; Margin:25px auto;'>
+<table width='800' align='center'>
+    <tr>
+        <td><![endif]-->
+<table border='0' cellpadding='0' cellspacing='0' width='100%'
+       style='max-width:800px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif; Margin:25px auto;'>
     <tr>
         <td>
             <table cellpadding='0' cellspacing='0' class="storehouse-table">
@@ -18,11 +21,19 @@
                 </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($channelValues as $channel => $value) { ?>
+                <?php foreach ($channelValues as $deviceID => $channels) { ?>
                     <tr>
-                        <td class="row" style='font-weight: bold;'><?= ucfirst($channel) ?></td>
-                        <td class="row"><?= round($value / 1000,3) ?>&nbsp;kW</td>
+                        <td class="row">&nbsp;</td>
+                        <td class="row">&nbsp;</td>
                     </tr>
+                    <?php foreach ($channels as $channelID => $value) { ?>
+                        <tr>
+                            <td class="row"
+                                style='font-weight: bold;'><?= $aliases[$deviceID][$channelID] ?? $channelID ?></td>
+                            <td class="row"><?= round($value / 1000, 3) ?>&nbsp;kW</td>
+                        </tr>
+                    <?php } ?>
+
                 <?php } ?>
                 </tbody>
             </table>
