d3369cc79a78437b9c061ea4a90ee9098b07d50a
Matej <matejchalachan@gmail.com>
2022-06-19 20:26:48 +0200

    fixed migration

--- src/migrations/m211110_203820_create_junction_table_for_price_map_and_subscriber_tables.php
+++ src/migrations/m211110_203820_create_junction_table_for_price_map_and_subscriber_tables.php
@@ -3,11 +3,11 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `{{%price_map_subscriber}}`.
+ * Handles the creation of table `{{%iot24_price_map_subscriber}}`.
  * Has foreign keys to the tables:
  *
- * - `{{%price_map}}`
- * - `{{%subscriber}}`
+ * - `{{%iot24_price_map}}`
+ * - `{{%iot24_subscriber}}`
  */
 class m211110_203820_create_junction_table_for_price_map_and_subscriber_tables extends Migration
 {
@@ -21,7 +21,7 @@ class m211110_203820_create_junction_table_for_price_map_and_subscriber_tables e
             $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('{{%price_map_subscriber}}', [
+        $this->createTable('{{%iot24_price_map_subscriber}}', [
             'price_map_id' => $this->integer(),
             'subscriber_id' => $this->integer(),
             'PRIMARY KEY(price_map_id, subscriber_id)',
@@ -29,21 +29,21 @@ class m211110_203820_create_junction_table_for_price_map_and_subscriber_tables e
 
         $this->createIndex(
             '{{%idx-price_map_subs-price_map_id}}',
-            '{{%price_map_subscriber}}',
+            '{{%iot24_price_map_subscriber}}',
             'price_map_id'
         );
 
         $this->createIndex(
             '{{%idx-price_map_subs-subscriber_id}}',
-            '{{%price_map_subscriber}}',
+            '{{%iot24_price_map_subscriber}}',
             'subscriber_id'
         );
 
         $this->addForeignKey(
             '{{%fk-price_map_subs-price_map_id}}',
-            '{{%price_map_subscriber}}',
+            '{{%iot24_price_map_subscriber}}',
             'price_map_id',
-            '{{%price_map}}',
+            '{{%iot24_price_map}}',
             'id',
             'CASCADE',
             'CASCADE'
@@ -51,9 +51,9 @@ class m211110_203820_create_junction_table_for_price_map_and_subscriber_tables e
 
         $this->addForeignKey(
             '{{%fk-price_map_subs-subscriber_id}}',
-            '{{%price_map_subscriber}}',
+            '{{%iot24_price_map_subscriber}}',
             'subscriber_id',
-            '{{%subscriber}}',
+            '{{%iot24_subscriber}}',
             'id',
             'CASCADE',
             'CASCADE'
@@ -67,24 +67,24 @@ class m211110_203820_create_junction_table_for_price_map_and_subscriber_tables e
     {
         $this->dropForeignKey(
             '{{%fk-price_map_subs-price_map_id}}',
-            '{{%price_map_subscriber}}'
+            '{{%iot24_price_map_subscriber}}'
         );
 
         $this->dropForeignKey(
             '{{%fk-price_map_subs-subscriber_id}}',
-            '{{%price_map_subscriber}}'
+            '{{%iot24_price_map_subscriber}}'
         );
 
         $this->dropIndex(
             '{{%idx-price_map_subs-price_map_id}}',
-            '{{%price_map_subscriber}}'
+            '{{%iot24_price_map_subscriber}}'
         );
 
         $this->dropIndex(
             '{{%idx-price_map_subs-subscriber_id}}',
-            '{{%price_map_subscriber}}'
+            '{{%iot24_price_map_subscriber}}'
         );
 
-        $this->dropTable('{{%price_map_subscriber}}');
+        $this->dropTable('{{%iot24_price_map_subscriber}}');
     }
 }
